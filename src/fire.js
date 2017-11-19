7053de0a690e2bd6ccdd5cf16f8a35338a63d555
NH\nhoon_000 <nhoon2002@ucla.edu>
2017-11-19 12:06:29 -0800

    edit on the fly

--- src/fire.js
+++ src/fire.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyD7LM-C250G8THaI3w7BV-9Zn1WVBBf7-k",
-  authDomain: "testingupload-22761.firebaseapp.com",
-  databaseURL: "https://testingupload-22761.firebaseio.com",
-  projectId: "testingupload-22761",
-  storageBucket: "testingupload-22761.appspot.com",
-  messagingSenderId: "422317099707"
+  apiKey: "AIzaSyBoPaRTqNCMV1An4_nKIWU3ycpCGFTIppw",
+  authDomain: "coupletest2-86369.firebaseapp.com",
+  databaseURL: "https://coupletest2-86369.firebaseio.com",
+  projectId: "coupletest2-86369",
+  storageBucket: "",
+  messagingSenderId: "171737948717"
 };
 
 var fire = firebase.initializeApp(config);
